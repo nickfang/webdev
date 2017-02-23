2c3c099ee5370a2de2058840a730af31b7d2d9f0
nfang <fang.nicholas@gmail.com>
2017-02-22 19:11:52 -0600

    The scrolling seems to be working.  There are several questions though.
    If I remove the 1px border, for some reason the image get's moved and
    starts at the top of the space-below.
    If I change the left and right margin from 10% to 20% on .top-bar and
    .main-picture, then the bottom bar doesn't start at the correct place
    anymore.
    
    I wanted for this to work no matter how you scale the window, but I'm
    starting to think I might want to keep the image a certain size and figure
    out the scrolling around that.

--- prototyping/pictureScroll/resources/js/pictureScroll.js
+++ prototyping/pictureScroll/resources/js/pictureScroll.js
@@ -1,20 +1,46 @@
-const barTop =  document.querySelector('.bar-top');
-const barBottom = document.querySelector('.bar-bottom');
-const picture = document.querySelector('.main-picture');
+/*
+	With google chrome half the width of a 2560
+	barTop.offsetTop = 9
+	barTop.offsetHeight = 374
+	picture.offsetTop = 383
+	picture.offsetHeight = 1247
+	spaceBelow.offsetTop = 1630
+	spaceBelow.offsetHeight = 374
 
-// let scrollHeight = picture.scrollHeight;
-// let scrollTop = picture.scrollTop;
+*/
+
+const barTop =  document.querySelector('.bar-top');
+const picture = document.querySelector('.main-picture');
+const barBottom = document.querySelector('.bar-bottom');
+const spaceBelow = document.querySelector('.space-below');
+
 
 function setBarSizes() {
-	let barHeight = .3 * (picture.clientHeight - picture.scrollTop);
+	let barHeight = Math.ceil(.3 * picture.clientHeight);
 	let barTopMargin = picture.clientHeight - barHeight;
-	barBottom.style.height = `${barHeight}px`;
 	barTop.style.height = `${barHeight}px`;
+	barBottom.style.height = `${barHeight}px`;
 	barBottom.style.marginTop = `${barTopMargin}px`;
-	console.log("clientHeight: " + picture.clientHeight + ", barHeight: ", barHeight + ", barTop: " + barTop);
+	spaceBelow.style.height = `${barHeight}px`;
+}
+
+function updateBottomBar() {
+	// need to be able to handle if the entire picture and space below is in view.
+	// if space-below is not in view, start figure out when it comes into view and move the bottom bar according to how much of space-below is showing.
+	let barHeight = Math.ceil(.3 * picture.clientHeight);
+	let barTopMargin = picture.clientHeight - barHeight;
+	windowPositionBottom = window.scrollY + window.innerHeight;
+	if (windowPositionBottom > spaceBelow.offsetTop && windowPositionBottom < spaceBelow.offsetTop + barHeight) {
+		barBottom.style.marginTop = `${barTopMargin + (windowPositionBottom - (picture.offsetTop + picture.offsetHeight))}px`
+	} else if (windowPositionBottom > (spaceBelow.offsetTop + barHeight)) {
+		barBottom.style.marginTop = `${picture.offsetHeight}px`;
+	}
+
 }
 
 setBarSizes();
-window.scrollTo(0,0);
-window.addEventListener('scroll', setBarSizes);
-window.onresize = setBarSizes;
+
+window.addEventListener('scroll', updateBottomBar);
+
+window.onresize = setBarSizes;  // make sure the bars are resized when the window is resized.
+window.onbeforeunload = () => window.scrollTo(0,0);  // set scrollTo to the top of the page so it will go there when the page is refreshed.
