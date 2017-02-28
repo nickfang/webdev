a21d72ec127a033ab9ad0b7dfb9ec28d53b37461
nfang <fang.nicholas@gmail.com>
2017-02-28 02:18:56 -0600

    I got the text shadow working and it also fades in with the text now. I found a case that we never hit, so I removed it.

--- prototyping/pictureScroll/resources/js/pictureScroll2.js
+++ prototyping/pictureScroll/resources/js/pictureScroll2.js
@@ -10,11 +10,9 @@ const spaceBelow = document.querySelector('.space-below');
 const text = document.querySelectorAll('.text');
 const textBottom = document.querySelector('.bar-bottom-text');
 
-
 let pictureHeight;
 let barHeight;
 
-// TODO: scale font depending on size of window.
 function initSizes() {
 	pictureHeight = Math.ceil(window.innerHeight / (1 + barPercent));
 	barHeight = Math.ceil(barPercent * pictureHeight);
@@ -22,6 +20,7 @@ function initSizes() {
 	picture.style.height = `${pictureHeight}px`;
 	// TODO:  figure out why the bar width is sometimes 0.
 	const pictureWidth = picture.width;
+	console.log(picture.width);
 	// center the image on the page
 	pictureWindow.style.width = `${pictureWidth}px`;
 
@@ -39,6 +38,8 @@ function initSizes() {
 	for (let i = 0; i < text.length; i++) {
 		text[i].style.fontSize = `${Math.floor(barHeight / 6)}px`;
 	}
+	// hide the text shadow
+	setTextBottomShadow(0);
 
 	// space for the bottom bar to scroll into.
 	spaceBelow.style.height = `${barHeight}px`;
@@ -53,21 +54,27 @@ function updateBottomBar() {
 		barBottom.style.top = `${windowPositionBottom - spaceBelow.offsetTop + barTopOriginal}px`;
 		// fade text in and out while scrolling.  Fade in after halfway through moving the bottom bar.
 		if (windowPositionBottom > spaceBelow.offsetTop + (barHeight / 2)) {
-			textBottom.style.color = `rgba(255,255,255,${(2 * (windowPositionBottom - spaceBelow.offsetTop) / barHeight) - 1})`;
+			// simplified equation for going from 0 at half bar height and 1 at full bar height
+			opacity = (2 * (windowPositionBottom - spaceBelow.offsetTop) / barHeight) - 1;
+			// I didn't like how the fade in looked since you saw the shadow first.  Made the text fade in twice as fast.
+			textBottom.style.color = `rgba(255,255,255,${opacity * 2})`;
+			setTextBottomShadow(opacity);
 		} else {
 			// if we're not fading in the text, make sure it's completely faded out.
 			textBottom.style.color = `rgba(255,255,255,0)`;
+			setTextBottomShadow(0);
 		}
-	} else if (windowPositionBottom > spaceBelow.offsetTop + barHeight) {
-		// If we scroll past the bottom make sure bar bottom is at final location and text is completely opaque.
-		barBottom.style.top = `${pictureHeight}px`;
-		textBottom.style.color = `rgba(255,255,255,1)`;
 	} else if (window.scrollY == 0) {
 		// if we're scrolled to the top of the page, make sure the bar is in the right location and the text is completely transparent
 		// if we don't do this, the bottom bar might not move all the way back to the original location.
 		barBottom.style.top = `${barTopOriginal}px`;
 		textBottom.style.color = `rgba(255,255,255,0)`;
+		setTextBottomShadow(0);
 	}
+}
+
+function setTextBottomShadow(opacity) {
+	textBottom.style.textShadow = `2px 2px 0 rgba(0,0,0,${opacity}), -2px 2px 0 rgba(0,0,0,${opacity}), 2px -2px 0 rgba(0,0,0,${opacity}), -2px -2px 0 rgba(0,0,0,${opacity})`
 }
 
 debugBorders ? border = "1px solid green" : border = "none";
