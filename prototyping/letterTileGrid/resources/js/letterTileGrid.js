8643ecb86b4986f6d36b408a373ce7aa57a00234
nfang <fang.nicholas@gmail.com>
2017-03-09 12:34:23 -0600

    I think I have the navigation part working correctly.  I'm going to do some refactoring now.

--- prototyping/letterTileGrid/resources/js/letterTileGrid.js
+++ prototyping/letterTileGrid/resources/js/letterTileGrid.js
@@ -1,11 +1,10 @@
 const boardSize = 10;
 const board = document.querySelector("#board");
 
-
-let xPos = 3;
-let yPos = 4;
-
+let xPos = 0;
+let yPos = 0;
 let horizHighlight = true;
+
 
 function createBoard(numTilesPerSide) {
 	const iDiv = document.createElement("div");
@@ -29,69 +28,105 @@ function createBoard(numTilesPerSide) {
 		tiles[i].style.width = `${tileSideLength}vw`
 		tiles[i].style.height = `${tileSideLength}vw`
 	}
+
+	// TODO: this line is used in several places.  figure out how to refactor
+	const cursor = document.querySelector(`.row-${yPos} .col-${xPos}`);
+	cursor.classList.add("cursor");
+	// TODO: this line is used in several places.  figure out how to refactor
+	const highlights = document.querySelectorAll(`.row-${yPos} .tile`);
+	drawHighlights(highlights);
 }
 
-function moveCursor(e){
+function keyHandler(e){
 	// up:    38
 	// down:  40
 	// left:  37
 	// right: 39
-	if (e.keyCode === 32 || e.keyCode === 37 || e.keyCode === 38 || e.keyCode === 39 || e.keyCode === 40) {
-		const cursor = document.querySelector(`.row-${yPos} .col-${xPos}`);
+	if (e.keyCode === 37 || e.keyCode === 38 || e.keyCode === 39 || e.keyCode === 40) {
+		let cursor = document.querySelector(`.row-${yPos} .col-${xPos}`);
 		cursor.classList.remove("cursor");
+		// if we're moving in the same direction as the highlight, we only need to update 2 divs.
+		// if we're moving in the perpendicular direction, we need to erase the previous line and
+		// 	create a new line of highlights.
 		switch(e.keyCode) {
-			case 32: //	spacebar
-				horizHighlight = !horizHighlight;
-				removeHighlight();
-				break;
 			case 37:	// left button
-				--xPos < 0 ? (xPos = boardSize-1) : "";
-				console.log(xPos);
+			case 39:	// right button
+				if (horizHighlight) {
+					cursor.classList.add("highlight");
+				}
+				if (e.keyCode === 37) {
+					--xPos < 0 ? (xPos = boardSize-1) : "";
+				} else {
+					++xPos > boardSize-1 ? (xPos = 0) : "";
+				}
+				if (!horizHighlight) {
+					removeHighlights();
+					highlights = document.querySelectorAll(`.col-${xPos}`);
+					drawHighlights(highlights);
+				}
 				break;
 			case 38:	// up button
-				--yPos < 0 ? (yPos = boardSize-1) : "";
-				break;
-			case 39:	// right button
-				++xPos > boardSize-1 ? (xPos = 0) : "";
-				break;
 			case 40:	// down button
-				++yPos > boardSize-1 ? (yPos = 0) : "";
+				if (!horizHighlight) {
+					cursor.classList.add("highlight");
+				}
+				if (e.keyCode === 38) {
+					--yPos < 0 ? (yPos = boardSize-1) : "";
+				} else {
+					++yPos > boardSize-1 ? (yPos = 0) : "";
+				}
+				if (horizHighlight) {
+					removeHighlights();
+					highlights = document.querySelectorAll(`.row-${yPos} .tile`);
+					drawHighlights(highlights);
+				}
 				break;
 			default:
 				break;
 		}
-		drawCursor();
+		cursor = document.querySelector(`.row-${yPos} .col-${xPos}`);
+		cursor.classList.remove("highlight");
+		cursor.classList.add("cursor");
+	}
+	// spacebar: 32
+	if (e.keyCode === 32) {
+		let highlights;
+		horizHighlight = !horizHighlight;
+		removeHighlights();
+		if (horizHighlight) {
+			highlights = document.querySelectorAll(`.row-${yPos} .tile`);
+		} else {
+			highlights = document.querySelectorAll(`.col-${xPos}`);
+		}
+		drawHighlights(highlights);
 	}
 }
 
 function drawCursor() {
 	const cursor = document.querySelector(`.row-${yPos} .col-${xPos}`);
-	let highlight;
-
-	cursor.classList.toggle("cursor");
-	if (cursor.classList.contains("highlight")) {
-		cursor.classList.remove("highlight");
-	}
-	if (horizHighlight) {
-		highlight = document.querySelectorAll(`.row-${yPos} .tile`);
-	} else {
-		highlight = document.querySelectorAll(`.col-${xPos}`);
-	}
-		updateHighlight(highlight, true);
+	cursor.classList.add("cursor");
 }
 
-// input: highlight - array of tile level divs that will be highlighted
-function drawHighlight(highlight, draw) {
-	for (let i = 0; i < highlight.length; i++) {
-		if (!highlight[i].classList.contains("cursor")) {
-			highlight[i].classList.add("highlight");
+// input: highlights - array of tile level divs that will be highlighted
+function drawHighlights(highlights) {
+	for (let i = 0; i < highlights.length; i++) {
+		// this is here since it would be inefficient to go through the list to remove the element with class cursor
+		// only to go through the list again to add the highlight class.
+		// usually it's better to have these functions be more generic, but I feel like this is an exception.
+		if (!highlights[i].classList.contains("cursor")) {
+			highlights[i].classList.add("highlight");
 		}
 	}
 }
 
 function removeHighlights() {
 	const highlights = document.querySelectorAll(".highlight");
-
+	// for (let i = 0; i < highlights.length; i++) {
+	// 	highlights[i].classList.remove("highlight");
+	// }
+	highlights.forEach( (item) => {
+		item.classList.remove("highlight");
+	});
 }
 
 function preventKeyScrolling(e) {
@@ -104,7 +139,7 @@ function preventKeyScrolling(e) {
 }
 // TODO: make sure the board is completely visable
 
-window.addEventListener('keyup', moveCursor);
+window.addEventListener('keyup', keyHandler);
 window.addEventListener('keydown', preventKeyScrolling);
 
 createBoard(boardSize);
