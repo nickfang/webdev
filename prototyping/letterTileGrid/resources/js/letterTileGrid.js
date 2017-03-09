74981d3f41ad853bf772e442e604527d7023f4fa
nfang <fang.nicholas@gmail.com>
2017-03-09 10:24:49 -0600

    Moving along with getting this working.  For some reason I couldn't get square boxes for the longest time.  I'm actually not sure what I did to get it to work, but it's working now.
    I have a couple of event listeners created to disable scrolling with the
    arrow and space bar and to move the cursor around.  Moving the cursor
    isn't working yet, but I have showing where the cursor is and highlighting
    the direction working.

--- prototyping/letterTileGrid/resources/js/letterTileGrid.js
+++ prototyping/letterTileGrid/resources/js/letterTileGrid.js
@@ -1,18 +1,97 @@
 const board = document.querySelector("#board");
 
-function createBoard(sideLength) {
+
+let xPos = 3;
+let yPos = 4;
+let horizDirection = true;
+
+function createBoard(numTilesPerSide) {
 	const iDiv = document.createElement("div");
-	iDiv.className = "row tile";
-	for (let i = 0; i < sideLength; i++) {
+	for (let i = 0; i < numTilesPerSide; i++) {
+		iDiv.className = `row-${i} row`;
 		board.appendChild(iDiv.cloneNode());
 	}
-	iDiv.className = "col tile";
 	boardRows = board.children;
 	for (let i = 0; i < boardRows.length; i++) {
-		for (let j = 0; j < sideLength; j++) {
+		for (let j = 0; j < numTilesPerSide; j++) {
+			iDiv.className = `col-${j} tile`;
 			boardRows[i].appendChild(iDiv.cloneNode());
+		}
+	}
+
+	// have to assign tiles here since it doesn't exist before now.
+	const tiles = document.querySelectorAll(".tile");
+	const tileSideLength = Math.floor((100 - 25 /* board margin is 10% on each side */) / numTilesPerSide);
+	for (let i = 0; i < tiles.length; i++) {
+		// tiles[i].innerHTML = i;
+		tiles[i].style.width = `${tileSideLength}vw`
+		tiles[i].style.height = `${tileSideLength}vw`
+	}
+}
+
+
+
+function moveCursor(e){
+	// up:    38
+	// down:  40
+	// left:  37
+	// right: 39
+	if (e.keyCode === 37 || e.keyCode === 38 || e.keyCode === 39 || e.keyCode === 40) {
+		switch(e.keyCode) {
+			case 37:
+				// left button
+
+				break;
+			case 38:
+				// up button
+				break;
+			case 39:
+				// right button
+				break;
+			case 40:
+				// down button
+		}
+		drawCursor();
+	}
+}
+
+function drawCursor() {
+	const cursor = document.querySelector(`.row-${xPos} .col-${yPos}`);
+	let highlight;
+
+	cursor.classList.toggle("cursor");
+	if (cursor.classList.contains("highlight")) {
+		cursor.classList.remove("highlight");
+	}
+	if (horizDirection) {
+		highlight = document.querySelectorAll(`.row-${xPos} .tile`);
+		drawHighlight(highlight);
+	} else {
+		highlight = document.querySelectorAll(`.col-${yPos}`);
+		drawHighlight(highlight);
+	}
+}
+
+// input: highlight - array of tile level divs.
+function drawHighlight(highlight) {
+	for (let i = 0; i < highlight.length; i++) {
+		if (!highlight[i].classList.contains("cursor")) {
+			highlight[i].classList.add("highlight");
 		}
 	}
 }
 
-createBoard(3);
+function preventKeyScrolling(e) {
+	switch(e.keyCode) {
+		case 37: case 38: case 39: case 40: 	// catch arrow keys
+		case 32: e.preventDefault(); break;		// catch spacebar
+		default: break;								// allow everything else.
+	}
+
+}
+// TODO: make sure the board is completely visable
+
+window.addEventListener('keyup', moveCursor);
+window.addEventListener('keydown', preventKeyScrolling);
+
+createBoard(10);
