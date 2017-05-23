4c9c53a7107991e84c3456e4ecb45fa9c805de84
nfang <fang.nicholas@gmail.com>
2017-05-23 18:53:35 -0500

    Updated abs to use some features from the learnNode class.

--- abs/app.js
+++ abs/app.js
@@ -1,21 +1,33 @@
 const port = 3000;
 
 var express 			= require("express");
-var bodyParser 		= require("body-parser");
+var bodyParser 		= require("body-parser");  		// https://github.com/expressjs/body-parser
 var mongoose			= require("mongoose");
-var methodOverride 	= require("method-override");
+var methodOverride 	= require("method-override");		// https://github.com/expressjs/method-override
 var User					= require("./models/user");
 var profile				= require("./models/profile");
+var helpers				= require("./helpers");
 var app 					= express();
 
 var indexRoutes = require("./routes/index");
 var profileRoutes = require("./routes/profile");
 
 mongoose.connect("mongodb://fangns-dev-shard-00-00-fhkfn.mongodb.net:27017,fangns-dev-shard-00-01-fhkfn.mongodb.net:27017,fangns-dev-shard-00-02-fhkfn.mongodb.net:27017/fangns-dev?ssl=true&replicaSet=fangns-dev-shard-0&authSource=admin"); //"" --ssl-username fangns --password fangns -authenticationDatabase admin
+
+// view engine setup
 app.set("view engine", "ejs");
+
+// serves up static files from the public folder
 app.use(express.static(__dirname + "/public"));
+
+
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+
 app.use(methodOverride("_method"));
+
+
 
 app.use("/", indexRoutes);
 app.use("/profiles", profileRoutes);
---
+++ abs/helpers.js
@@ -0,0 +1,4 @@
+// exports helpers that are available to the whole site
+
+// global site details
+exports.siteName = "Austin Background Society";
